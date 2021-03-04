b4c62771ce6893112d39fb41ee846fcfeb0d2cba
chrisndodge <cdodge2016@gmail.com>
2021-03-04 11:04:55 -0500

    Fix imports

--- tinkerpop/cypher-gremlin-extensions/src/main/java/org/opencypher/gremlin/traversal/CustomFunctions.java
+++ tinkerpop/cypher-gremlin-extensions/src/main/java/org/opencypher/gremlin/traversal/CustomFunctions.java
@@ -35,7 +35,6 @@ import java.util.Calendar;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
-import java.util.LocalDate;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
